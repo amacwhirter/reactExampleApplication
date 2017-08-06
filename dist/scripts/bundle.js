873f972fafe2f04862a7abbba847bebc4a8790a1
Ashley MacWhirter <macwhirtera@gmail.com>
2017-08-05 23:50:04 -0400

    update

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -29147,6 +29147,31 @@ module.exports = About;
 
 var React = require('react');
 
+var Header = React.createClass({displayName: "Header",
+  render: function() {
+    return(
+      React.createElement("nav", {className: "navbar navbar-default"}, 
+        React.createElement("div", {className: "container-fluid"}, 
+          React.createElement("a", {href: "/", className: "navbar-brand"}, 
+            React.createElement("img", {src: "images/admlogo.png", height: "30px"})
+          ), 
+          React.createElement("ul", {className: "nav navbar-nav"}, 
+            React.createElement("li", null, React.createElement("a", {href: "/"}, "Home")), 
+            React.createElement("li", null, React.createElement("a", {href: "/#about"}, "About"))
+          )
+        )
+      )
+    );
+  }
+});
+
+module.exports = Header;
+
+},{"react":157}],160:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+
 var Home = React.createClass({displayName: "Home",
   render: function() {
     return(
@@ -29160,11 +29185,12 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":157}],160:[function(require,module,exports){
+},{"react":157}],161:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
 var App = React.createClass({displayName: "App",
   render: function() {
@@ -29177,6 +29203,7 @@ var App = React.createClass({displayName: "App",
 
     return (
       React.createElement("div", null, 
+        React.createElement(Header, null), 
         React.createElement(Child, null)
       )
     );
@@ -29191,4 +29218,4 @@ function render() {
 window.addEventListener('hashchange', render);
 render();
 
-},{"./components/about/aboutPage":158,"./components/homePage":159,"jquery":1,"react":157}]},{},[160]);
+},{"./components/about/aboutPage":158,"./components/common/header":159,"./components/homePage":160,"jquery":1,"react":157}]},{},[161]);
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var config = {
   paths: {
     html: './src/*.html',
     js: '.src/**/*.js',
+    images: './src/images/*',
     css: [
       'node_modules/bootstrap/dist/css/bootstrap.min.css',
       'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -62,6 +63,18 @@ gulp.task('css', function() {
     .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
+//Migrates images to dist folder
+//Could even optimize images here
+gulp.task('images', function() {
+  gulp.src(config.paths.images)
+    .pipe(gulp.dest(config.paths.dist + '/images'))
+    .pipe(connect.reload());
+
+//publish favicon
+  gulp.src('./src/favicon.ico')
+    .pipe(gulp.dest(config.paths.dist));
+});
+
 gulp.task('lint', function() {
   return gulp.src(config.paths.js)
     .pipe(lint({config: 'eslint.config.json'}))
@@ -74,4 +87,4 @@ gulp.task('watch', function() {
   gulp.watch(config.paths.css, ['css']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
